97e8f2f4212ba915a96d26d1675bfe04af51a43c
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2025-07-22 14:49:47 -0300

    Fix decoder build on windows

--- decoder_server/setup.py
+++ decoder_server/setup.py
@@ -12,19 +12,19 @@ cpp_sources = [file for file in cpp_sources if not file.endswith("_decoder.cpp")
 extensions = [
     Extension(
         "decoder.cython.eyeswipe_decoder",
-        ["decoder/cython/eyeswipe_decoder.pyx"] + cpp_sources,
+        ["decoder/cython/eyeswipe_decoder.pyx", "decoder/cython/eyeswipeDecoder.cpp", "decoder/cython/prefixTree.cpp", "decoder/cython/utils.cpp"],
         extra_compile_args=compiler_args,
         language="c++"
     ),
     Extension(
         "decoder.cython.suffix_decoder",
-        ["decoder/cython/suffix_decoder.pyx"] + cpp_sources,
+        ["decoder/cython/suffix_decoder.pyx", "decoder/cython/suffixDecoder.cpp", "decoder/cython/prefixTree.cpp", "decoder/cython/utils.cpp"],
         extra_compile_args=compiler_args,
         language="c++"
     ),
     Extension(
         "decoder.cython.trie",
-        ["decoder/cython/trie.pyx"] + cpp_sources,
+        ["decoder/cython/trie.pyx", "decoder/cython/trie.cpp", "decoder/cython/prefixTree.cpp"],
         extra_compile_args=compiler_args,
         language="c++"
     )
@@ -36,4 +36,5 @@ setup(
         extensions,
         compiler_directives={'language_level': 3}
     ),
+    py_modules=["decoder"],
 )
